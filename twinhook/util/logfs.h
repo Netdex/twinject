8e7c4f5e6fda8f899a436fe29d9463eaad55764e
netdex <netdex593@gmail.com>
2018-11-18 11:41:24 -0500

    log: formatting

--- twinhook/util/logfs.h
+++ twinhook/util/logfs.h
@@ -4,6 +4,6 @@
 #define TOSTRING(x) STRINGIFY(x)
 
 #define LOG(fmt, ...) logm(__FILE__, __func__, __LINE__, fmt, ##__VA_ARGS__)
-#define ASSERT(a) do{if(!(a)){LOG("assertion failed: " TOSTRING(a)); exit(1);}}while(0)
+#define ASSERT(a) do{if(!(a)){LOG("Assertion Failed: " TOSTRING(a)); exit(1);}}while(0)
 
 void logm(const char* file, const char* func, int line, const char* fmt, ...);
