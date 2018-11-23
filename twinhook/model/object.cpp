e85654d4a5d0fee011b9b887d36156b2c2f8acfb
netdex <netdex593@gmail.com>
2018-11-22 20:43:44 -0500

    model: fix build error

--- twinhook/model/object.cpp
+++ twinhook/model/object.cpp
@@ -4,10 +4,11 @@ float aabb::willCollideWith(const entity& o)
 {
 	switch (o.type)
 	{
-	case AABB:
+	case AABB: {
 		const auto& a = dynamic_cast<const aabb&>(o);
 		return vec2::willCollideAABB(position, a.position,
 			size, a.size, velocity, a.velocity);
+	}
 	default: return -1.f;
 	}
 }
@@ -16,10 +17,11 @@ float circle::willCollideWith(const entity& o)
 {
 	switch (o.type)
 	{
-	case Circle:
+	case Circle: {
 		const auto& a = dynamic_cast<const circle&>(o);
 		return vec2::willCollideCircle(center, a.center,
 			radius, a.radius, velocity, a.velocity);
+	}
 	default: return -1.f;
 	}
 }
@@ -28,9 +30,10 @@ float polygon::willCollideWith(const entity& o)
 {
 	switch (o.type)
 	{
-	case Polygon:
+	case Polygon: {
 		const auto& a = dynamic_cast<const polygon&>(o);
 		return vec2::willCollideSAT(points, velocity, a.points, a.velocity);
+	}
 	default: return -1.f;
 	}
 }
